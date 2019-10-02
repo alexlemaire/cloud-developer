0f9442f959b7a3a4d29522dbbf7eea981ac18c2d
Alexis Lemaire <a.lemaire@klimapartner.de>
2019-10-02 23:29:57 +0200

    Updated server.ts to be able to build

--- course-02/exercises/udacity-c2-restapi/src/server.ts
+++ course-02/exercises/udacity-c2-restapi/src/server.ts
@@ -20,7 +20,7 @@ import { V0MODELS } from './controllers/v0/model.index';
   //CORS Should be restricted
   app.use(function(req, res, next) {
     if (req.headers.origin === 'http://localhost:8080/api/v0' || 'http://udagram-lemaire-dev-dev.eu-central-1.elasticbeanstalk.com/api/v0' || 'http://udagram-lemaire.eu-central-1.elasticbeanstalk.com/api/v0') {
-      res.header("Access-Control-Allow-Origin", req.headers.origin);
+      res.header("Access-Control-Allow-Origin", `${req.headers.origin}`);
     }
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
