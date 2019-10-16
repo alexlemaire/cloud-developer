b46d25de773cef88a0ddffe9478859b5ef2d467d
Alexis Lemaire <a.lemaire@klimapartner.de>
2019-10-16 15:19:54 +0200

    Created lambda for creating groups in DynamoDB

--- course-04/exercises/lesson-2/create-item-api/index.js
+++ course-04/exercises/lesson-2/create-item-api/index.js
@@ -1,24 +1,30 @@
 const AWS = require('aws-sdk')
+const uuid = require('uuid')
 
 const docClient = new AWS.DynamoDB.DocumentClient()
-
 const groupsTable = process.env.GROUPS_TABLE
 
 exports.handler = async (event) => {
+  const itemId = uuid.v4()
+  const parsedBody = JSON.parse(event.body)
 
-    const result = await docClient.scan({
-        TableName: groupsTable
-    }).promise()
+  const newItem = {
+    id: itemId,
+    ...parsedBody
+  }
 
-    const items = result.Items
+  await docClient.put({
+    TableName: groupsTable,
+    Item: newItem
+  }).promise()
 
-    return {
-        statusCode: 200,
-        headers: {
-            'Access-Control-Allow-Origin': '*'
-        },
-        body: JSON.stringify({
-            items
-        })
-    }
+  return {
+      statusCode: 201,
+      headers: {
+          'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+          newItem
+      })
+  }
 };
