254c74834593ef15cf6d461b65da33e87f887654
Alexis Lemaire <a.lemaire@klimapartner.de>
2019-10-16 17:45:54 +0200

    Pagination exercise

--- course-04/exercises/lesson-2/starter-code/index.js
+++ course-04/exercises/lesson-2/starter-code/index.js
@@ -9,21 +9,33 @@ const groupsTable = process.env.GROUPS_TABLE
 exports.handler = async (event) => {
   console.log('Processing event: ', event)
 
-  // TODO: Read and parse "limit" and "nextKey" parameters from query parameters
-  // let nextKey // Next key to continue scan operation if necessary
-  // let limit // Maximum number of elements to return
+  // Read and parse "limit" and "nextKey" parameters from query parameters
+  let nextKey = getQueryParameter(event, 'nextKey') // Next key to continue scan operation if necessary
+  if (!nextKey) {
+    nextKey = null
+  } else {
+    nextKey = JSON.parse(decodeURIComponent(nextKey))
+  }
+  let limit = getQueryParameter(event, 'limit') // Maximum number of elements to return
 
-  // HINT: You might find the following method useful to get an incoming parameter value
-  // getQueryParameter(event, 'param')
-
-  // TODO: Return 400 error if parameters are invalid
+  // Return 400 error if parameters are invalid
+  if ((limit <= 0 && limit) || (nextKey <= 0 && nextKey)) {
+    return {
+      statusCode: 400,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        message: 'Invalid parameters specified'
+      })
+    }
+  }
 
   // Scan operation parameters
   const scanParams = {
     TableName: groupsTable,
-    // TODO: Set correct pagination parameters
-    // Limit: ???,
-    // ExclusiveStartKey: ???
+    Limit: limit,
+    ExclusiveStartKey: nextKey
   }
   console.log('Scan params: ', scanParams)
 
